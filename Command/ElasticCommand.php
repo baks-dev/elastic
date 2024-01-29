79d6a0e81cdf531a6d8e564f5ecd1ef8d27c5002
kingstar <admin@baks.dev>
2024-01-29 13:03:28 +0300

    BaksDevelopment

--- Command/ElasticCommand.php
+++ Command/ElasticCommand.php
@@ -12,26 +12,15 @@
 namespace BaksDev\Elastic\Command;
 
 
-use BaksDev\Core\Doctrine\ORMQueryBuilder;
-use BaksDev\Elastic\Api\ElasticInfo;
-use BaksDev\Elastic\Api\Index\ElasticDeleteByQueryIndex;
 use BaksDev\Elastic\Api\Index\ElasticDeleteIndex;
-use BaksDev\Elastic\Api\Index\ElasticGetIndex;
 use BaksDev\Elastic\Api\Index\ElasticSetIndex;
 use BaksDev\Elastic\Api\Mappings\ElasticGetMap;
 use BaksDev\Elastic\Api\Mappings\ElasticSetMap;
-use BaksDev\Elastic\Api\Properties\IntegerElasticType;
-use BaksDev\Elastic\Api\Properties\KeywordElasticType;
-use BaksDev\Elastic\Api\Properties\TextElasticType;
-use BaksDev\Elastic\Index\ElasticIndexInterface;
-use Doctrine\ORM\Mapping\ClassMetadata;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\DependencyInjection\Attribute\TaggedIterator;
 
 #[AsCommand(
     name: 'baks:elastic:index',
